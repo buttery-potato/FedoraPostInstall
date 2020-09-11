860db4eceed0b7c2f19b019e6a21fd54c2b3a40c
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-09-11 10:25:11 -0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# post install script for Fedora Workstation Edition
+
+sudo su
+
+# upgrade and install packages
+dnf upgrade -y
+dnf install fish ufw neofetch gnome-tweaks util-linux-user htop inxi
+
+# configure ufw
+systemctl disable --now firewalld
+systemctl enable --now  ufw
+ufw enable
+ufw default deny incoming
+ufw default allow outgoing
+
+echo "Thanks for using my post-install script. If there were any bugs, please report them at my github page at https://www.github.com/buttery-potato/Fedora-Post-Install-Script/issues.
+
+
