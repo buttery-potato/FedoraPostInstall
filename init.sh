cd3e8d8443646cabc00810aa720d0f26199b6e74
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-09-12 10:07:11 -0700

    Update and rename script.sh to init.sh
    Update to create separate file to simultaneously configure firewall

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# post install script for Fedora Workstation Edition
+
+
+# upgrade and install packages
+sudo dnf upgrade -y
+sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop inxi -y
+
+sh ./firewall_config.sh
