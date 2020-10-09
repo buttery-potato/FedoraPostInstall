1348c4531dece6ca9b71ab43a4b62d2873908e37
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-10-08 21:56:15 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@
 # upgrade and install packages
 sudo dnf upgrade -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop inxi deluge flat-remix-theme -y
+sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop inxi rust rust-doc cargo atom -y
 
 sudo sh ./config.sh
