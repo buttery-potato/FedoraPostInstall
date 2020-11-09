c6018b84894cc48e36c8350def13500c4bcfc93f
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-11-09 11:39:49 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,6 +6,6 @@ sudo ufw enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-echo "Thanks for using my post-install script. If there were any bugs, please report them at my github page at https://www.github.com/buttery-potato/Fedora-Post-Install-Script/issues."
+
 
 
