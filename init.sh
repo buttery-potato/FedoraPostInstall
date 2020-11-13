806a3cb303bcabb19b9f4a8eebe0eddc2e6a8a0c
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-11-13 11:20:54 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
 # post install script for Fedora Workstation Edition
-
 
 # upgrade and install packages
 sudo dnf upgrade -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop flat-remix-theme inxi -y
-
-sudo sh ./config.sh
+#configure firewall
+sudo systemctl disable --now firewalld
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
