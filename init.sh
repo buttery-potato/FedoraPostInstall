3c29d5a9d1adb1da04166baf0d158ae86c5f0f9c
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-11-13 14:52:10 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@
 sudo dnf upgrade -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop flat-remix-theme inxi flameshot -y
+sudo dnf group upgrade multimedia
 #configure firewall
 sudo systemctl disable --now firewalld
 sudo ufw enable
