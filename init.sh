294a7302bfbbf60bf2945bba25a912accfd9b39d
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2020-11-13 21:04:54 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,9 @@
 # upgrade and install packages
 sudo dnf upgrade -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop flat-remix-theme inxi flameshot -y
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install fish ufw neofetch gnome-tweaks util-linux-user htop flat-remix-theme inxi flameshot code -y
 sudo dnf group upgrade multimedia
 #configure firewall
 sudo systemctl disable --now firewalld
